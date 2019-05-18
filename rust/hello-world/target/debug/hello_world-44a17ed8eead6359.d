4bf606297454c8115fd46975e7775eec8a3f3e41
Corey Alexander <coreyja@gmail.com>
2019-05-17 20:45:11 -0400

    Do first rust challenge (maybe again)

--- rust/hello-world/target/debug/hello_world-44a17ed8eead6359.d
+++
@@ -1,5 +0,0 @@
-/Users/coreyja/exercism/rust/hello-world/target/debug/hello_world-44a17ed8eead6359.d: tests/hello-world.rs
-
-/Users/coreyja/exercism/rust/hello-world/target/debug/hello_world-44a17ed8eead6359: tests/hello-world.rs
-
-tests/hello-world.rs:
