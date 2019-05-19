f7fe35a4d63d6cac6296a789a0c528aec65316ce
Corey Alexander <coreyja@gmail.com>
2019-05-19 17:18:52 -0400

    Complete clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,39 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Clock {
+    hour: i32,
+    minute: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let minute_rollover = minutes / 60;
+        let mut minute = minutes % 60;
+        let mut hour = (hours + minute_rollover) % 24;
+        while minute < 0 {
+            minute += 60;
+            hour -= 1;
+        }
+        while hour < 0 {
+            hour += 24;
+        }
+        Clock { hour, minute }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Self::new(self.hour, self.minute + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hour, self.minute)
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Clock) -> bool {
+        self.hour == other.hour && self.minute == other.minute
     }
 }
